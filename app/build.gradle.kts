80e22276d046535e4aedaeee66999996856a22e2
yusufjamil2009-sudo <yusufjamil2009@gmail.com>
2026-09-17 13:39:52 +0530

    Part 01: add unit-test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
+    testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
