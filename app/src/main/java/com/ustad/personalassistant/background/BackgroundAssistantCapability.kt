daa5dd8e8e953c1b4e679ef4aa17ccf63ed22fe7
yusufjamil2009-sudo <yusufjamil2009@gmail.com>
2026-09-17 13:46:08 +0530

    feat: add Android background assistant capability foundation

---
+++ app/src/main/java/com/ustad/personalassistant/background/BackgroundAssistantCapability.kt
@@ -0,0 +1,19 @@
+package com.ustad.personalassistant.background
+
+import android.app.ActivityManager
+import android.content.Context
+import android.os.PowerManager
+
+interface BackgroundAssistantCapability {
+    fun foregroundServiceSupported(): Boolean
+    fun batteryOptimizationRelevant(): Boolean
+}
+
+class AndroidBackgroundAssistantCapability(private val context: Context) : BackgroundAssistantCapability {
+    override fun foregroundServiceSupported(): Boolean = true
+
+    override fun batteryOptimizationRelevant(): Boolean {
+        val power = context.getSystemService(Context.POWER_SERVICE) as? PowerManager ?: return false
+        return !power.isIgnoringBatteryOptimizations(context.packageName)
+    }
+}
