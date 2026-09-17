d5416bc24c99ea223c14682d20a58d2b9e64dde9
yusufjamil2009-sudo <yusufjamil2009@gmail.com>
2026-09-17 13:58:05 +0530

    feat: expose automation foundations through existing application graph

--- app/src/main/java/com/ustad/personalassistant/UstadApplication.kt
+++ app/src/main/java/com/ustad/personalassistant/UstadApplication.kt
@@ -1,24 +1,33 @@
 package com.ustad.personalassistant
 
 import android.app.Application
+import com.ustad.personalassistant.accessibility.AccessibilityActionEngine
+import com.ustad.personalassistant.accessibility.AndroidAccessibilityActionEngine
+import com.ustad.personalassistant.accessibility.DefaultAutomationPolicy
+import com.ustad.personalassistant.accessibility.AutomationPolicy
 import com.ustad.personalassistant.capability.CapabilityEngine
 import com.ustad.personalassistant.capability.DefaultCapabilityEngine
 import com.ustad.personalassistant.data.AppStateRepositoryImpl
 import com.ustad.personalassistant.data.SettingsRepositoryImpl
+import com.ustad.personalassistant.gmail.DefaultGmailService
+import com.ustad.personalassistant.gmail.GmailService
+import com.ustad.personalassistant.gmail.SecureGmailAuthStateStore
+import com.ustad.personalassistant.gmail.UnconfiguredGmailAuthManager
+import com.ustad.personalassistant.gmail.UnconfiguredGmailRepository
 import com.ustad.personalassistant.permissions.AndroidPermissionManager
+import com.ustad.personalassistant.security.SecureConfigStore
 import com.ustad.personalassistant.security.SecurityManagerImpl
+import com.ustad.personalassistant.services.UstadAccessibilityService
 
 class UstadApplication : Application() {
-    lateinit var permissionManager: AndroidPermissionManager
-        private set
-    lateinit var capabilityEngine: CapabilityEngine
-        private set
-    lateinit var appStateRepository: AppStateRepositoryImpl
-        private set
-    lateinit var settingsRepository: SettingsRepositoryImpl
-        private set
-    lateinit var securityManager: SecurityManagerImpl
-        private set
+    lateinit var permissionManager: AndroidPermissionManager; private set
+    lateinit var capabilityEngine: CapabilityEngine; private set
+    lateinit var appStateRepository: AppStateRepositoryImpl; private set
+    lateinit var settingsRepository: SettingsRepositoryImpl; private set
+    lateinit var securityManager: SecurityManagerImpl; private set
+    lateinit var automationPolicy: AutomationPolicy; private set
+    lateinit var accessibilityActionEngine: AccessibilityActionEngine; private set
+    lateinit var gmailService: GmailService; private set
 
     override fun onCreate() {
         super.onCreate()
@@ -27,5 +36,9 @@ class UstadApplication : Application() {
         appStateRepository = AppStateRepositoryImpl(permissionManager)
         settingsRepository = SettingsRepositoryImpl(this)
         securityManager = SecurityManagerImpl()
+        automationPolicy = DefaultAutomationPolicy()
+        accessibilityActionEngine = AndroidAccessibilityActionEngine({ UstadAccessibilityService.active }, securityManager)
+        val gmailState = SecureGmailAuthStateStore(SecureConfigStore(this, "gmail_auth_state"))
+        gmailService = DefaultGmailService(UnconfiguredGmailRepository(), UnconfiguredGmailAuthManager(gmailState))
     }
 }
