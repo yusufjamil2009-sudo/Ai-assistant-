3f29ca86f07b186158267e532879e89dc1433683
yusufjamil2009-sudo <yusufjamil2009@gmail.com>
2026-09-17 13:37:17 +0530

    Part 01: add verified application state models

---
+++ app/src/main/java/com/ustad/personalassistant/domain/AppState.kt
@@ -0,0 +1,27 @@
+package com.ustad.personalassistant.domain
+
+enum class CapabilityStatus {
+    OFF, ON, CONNECT, CONNECTED, NOT_AVAILABLE, ACTION_REQUIRED
+}
+
+data class AppState(
+    val assistantEnabled: Boolean = true,
+    val microphonePermission: CapabilityStatus = CapabilityStatus.OFF,
+    val voiceAuthentication: CapabilityStatus = CapabilityStatus.NOT_AVAILABLE,
+    val notificationAccess: CapabilityStatus = CapabilityStatus.OFF,
+    val accessibilityAccess: CapabilityStatus = CapabilityStatus.OFF,
+    val phoneCapability: CapabilityStatus = CapabilityStatus.OFF,
+    val contactsPermission: CapabilityStatus = CapabilityStatus.OFF,
+    val cameraPermission: CapabilityStatus = CapabilityStatus.OFF,
+    val locationPermission: CapabilityStatus = CapabilityStatus.OFF,
+    val filesCapability: CapabilityStatus = CapabilityStatus.ON,
+    val backgroundAssistantStatus: CapabilityStatus = CapabilityStatus.NOT_AVAILABLE,
+    val gmailConnection: CapabilityStatus = CapabilityStatus.CONNECT,
+    val googleAccountConnection: CapabilityStatus = CapabilityStatus.CONNECT
+)
+
+data class CapabilityInfo(
+    val title: String,
+    val description: String,
+    val status: CapabilityStatus
+)
