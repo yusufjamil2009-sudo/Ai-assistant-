026e74c2bb1315a9bb0ef166cd5d54b6a1cb6226
yusufjamil2009-sudo <yusufjamil2009@gmail.com>
2026-09-17 13:38:59 +0530

    Part 01: add notification access service foundation

---
+++ app/src/main/java/com/ustad/personalassistant/services/UstadNotificationListenerService.kt
@@ -0,0 +1,7 @@
+package com.ustad.personalassistant.services
+
+import android.service.notification.NotificationListenerService
+
+class UstadNotificationListenerService : NotificationListenerService() {
+    // Part 01: service registration only. No notification automation is performed yet.
+}
