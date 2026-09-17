f1aa46dc14fe41fe46234c8398a32316d1ff807a
yusufjamil2009-sudo <yusufjamil2009@gmail.com>
2026-09-17 13:47:14 +0530

    feat: expose centralized capability engine from application root

--- app/src/main/java/com/ustad/personalassistant/UstadApplication.kt
+++ app/src/main/java/com/ustad/personalassistant/UstadApplication.kt
@@ -1,6 +1,8 @@
 package com.ustad.personalassistant
 
 import android.app.Application
+import com.ustad.personalassistant.capability.CapabilityEngine
+import com.ustad.personalassistant.capability.DefaultCapabilityEngine
 import com.ustad.personalassistant.data.AppStateRepositoryImpl
 import com.ustad.personalassistant.data.SettingsRepositoryImpl
 import com.ustad.personalassistant.permissions.AndroidPermissionManager
@@ -8,6 +10,8 @@ import com.ustad.personalassistant.security.SecurityManagerImpl
 
 class UstadApplication : Application() {
     lateinit var permissionManager: AndroidPermissionManager
+        private set
+    lateinit var capabilityEngine: CapabilityEngine
         private set
     lateinit var appStateRepository: AppStateRepositoryImpl
         private set
@@ -19,6 +23,7 @@ class UstadApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         permissionManager = AndroidPermissionManager(this)
+        capabilityEngine = DefaultCapabilityEngine(permissionManager)
         appStateRepository = AppStateRepositoryImpl(permissionManager)
         settingsRepository = SettingsRepositoryImpl(this)
         securityManager = SecurityManagerImpl()
