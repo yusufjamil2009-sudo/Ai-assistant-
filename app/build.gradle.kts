e71cfc52f999efa174f2be2385e4bd1ae3600431
yusufjamil2009-sudo <yusufjamil2009@gmail.com>
2026-09-23 16:09:33 +0530

    Add Compose close icon dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,5 +29,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-core")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
